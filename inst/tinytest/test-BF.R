d455b2fb33011d43ec5aa20d4417988dd3544ba3
SamCH93 <samuel.pawel@gmail.com>
2023-06-28 10:47:40 +0200

    disable BFslogOR and BFsSMD tests (take too long)

--- inst/tinytest/test-BF.R
+++ inst/tinytest/test-BF.R
@@ -140,12 +140,12 @@ logORtable <- structure(list(study = c("Balafoutas and Sutter (2012), Science",
                                           0.0000595783977455087)),
                         class = "data.frame", row.names = c(NA, -3L))
 
-## BFslogOR tests (take too long, don't run by default)
-bfslogor1 <- with(logORtable, BFslogOR(ao = ao, bo = bo, co = co, do = do,
-                                       ar = ar, br = br, cr = cr, dr = dr,
-                                       method = "integration"))
-expect_equal(bfslogor1, logORtable$bfsint,
-             info = "BFslogOR (integration) as in Table 1 from Pawel and Held (2022)")
+## ## BFslogOR tests (take too long, don't run by default)
+## bfslogor1 <- with(logORtable, BFslogOR(ao = ao, bo = bo, co = co, do = do,
+##                                        ar = ar, br = br, cr = cr, dr = dr,
+##                                        method = "integration"))
+## expect_equal(bfslogor1, logORtable$bfsint,
+##              info = "BFslogOR (integration) as in Table 1 from Pawel and Held (2022)")
 ## bfslogor2 <- with(logORtable, BFslogOR(ao = ao, bo = bo, co = co, do = do,
 ##                                        ar = ar, br = br, cr = cr, dr = dr,
 ##                                        method = "hypergeo"))
@@ -158,6 +158,8 @@ bfrlogor1 <- with(logORtable, BFrlogOR(ao = ao, bo = bo, co = co, do = do,
                                        method = "integration", ss = 0))
 expect_equal(bfrlogor1, logORtable$bfrepint,
              info = "BFrlogOR (integration) as in Table 1 from Pawel and Held (2022)")
+## ## disable tests with method = "hypergeo" because causes issues on CRAN's M1
+## ## Mac and "integration" is anyway the default of BFrlogOR
 ## bfrlogor2 <- with(logORtable, BFrlogOR(ao = ao, bo = bo, co = co, do = do,
 ##                                        ar = ar, br = br, cr = cr, dr = dr,
 ##                                        method = "hypergeo", ss = 0))
